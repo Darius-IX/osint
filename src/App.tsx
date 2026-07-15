8d7a5f4f9715511ef2feb312f2f543d155055e89
Darius-IX <89026567+Darius-IX@users.noreply.github.com>
2026-07-15 16:16:27 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app">
+      <div className="navbar">
+        <p>Ethics</p>
+        <p>Data Collection</p>
+        <p>Data Proecessing</p>
+        <p>Results</p>
+      </div>
+      <div className="content">
+        <p>Welcome to the OSINT Project of Us</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
