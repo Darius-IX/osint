ef4126088712f709f1b61c68faff239b9eb6fc84
Darius-IX <89026567+Darius-IX@users.noreply.github.com>
2026-07-16 00:51:54 +0200

    Add ethics basics

--- src/pages/Ethics.tsx
+++ src/pages/Ethics.tsx
@@ -1,8 +1,29 @@
 const Ethics = () => {
   return (
-    <main>
-      <div>Ethics</div>
-    </main>
+    <div>
+      <h1>Ethics</h1>
+      <h2>Definition</h2>
+      <p>
+        analysis and incorporation of moral principles and norms into
+        decision-making
+      </p>
+      <h2>Considerations</h2>
+      <p>
+        <ul>
+          <li>
+            Do not trust the results blindly, double check with currently
+            accepted methods. The core idea is to identify false behaviour by
+            companies as a first entry point for further investigations.
+          </li>
+          <li>
+            Keep an eye on repurposing: the methodology could be abused for bad
+            things. Higher resolution satellite images might even enable to
+            analyse gardens of individuals or even military locations due to
+            their impact on the environment.
+          </li>
+        </ul>
+      </p>
+    </div>
   );
 };
 
